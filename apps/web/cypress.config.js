ce189d4bb96274b62f72512411e0adc320f612e6
Chia1104 <yuyuchia7423@gmail.com>
2022-10-18 11:48:23 +0800

    :construction_worker: Update `web` CI system.

--- apps/web/cypress.config.js
+++ apps/web/cypress.config.js
@@ -1,7 +1,7 @@
-module.exports = {
+import { defineConfig } from "cypress";
+
+export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
   },
-};
+});
