fada868ccf12f00413428a22d9a47cdf27353209
Chia1104 <yuyuchia7423@gmail.com>
2022-10-31 18:46:37 +0800

    :test_tube: Testing.

--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,7 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter, createContext } from "@wanin/trpc-api";
 
+// @ts-ignore
 export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
