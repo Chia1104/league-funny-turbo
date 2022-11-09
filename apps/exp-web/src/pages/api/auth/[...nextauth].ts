17b0b9901090de834861816edbb5f68509434426
Chia1104 <yuyuchia7423@gmail.com>
2022-11-10 00:33:00 +0800

    :construction:`IORedisAdapter` work in progress.

--- apps/exp-web/src/pages/api/auth/[...nextauth].ts
+++ apps/exp-web/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,14 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import TwitchProvider from "next-auth/providers/twitch";
 import FacebookProvider from "next-auth/providers/facebook";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  // @ts-ignore
+  url: process.env.UPSTASH_REDIS_URL,
+  token: process.env.UPSTASH_REDIS_TOKEN,
+});
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -13,6 +21,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
     }),
   ],
+  adapter: UpstashRedisAdapter(redis),
   session: { strategy: "jwt" },
   // jwt: {
   //   async encode({ secret, token }) {
