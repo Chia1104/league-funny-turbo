f12855d36629661c4098ab930920c951e18ca26d
Chia1104 <yuyuchia7423@gmail.com>
2022-11-09 18:40:28 +0800

    :construction: Work in progress.

--- apps/exp-web/src/pages/api/auth/[...nextauth].ts
+++ apps/exp-web/src/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,8 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import TwitchProvider from "next-auth/providers/twitch";
 import FacebookProvider from "next-auth/providers/facebook";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
-// import { prisma } from "@wanin/db";
 
 export const authOptions: NextAuthOptions = {
-  // callbacks: {
-  //   session({ session, user }) {
-  //     if (session.user) {
-  //       // @ts-ignore
-  //       session.user.id = user.id;
-  //     }
-  //     return session;
-  //   },
-  // },
-  // adapter: PrismaAdapter(prisma),
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID as string,
@@ -25,10 +13,15 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.TWITCH_CLIENT_SECRET as string,
     }),
   ],
-  // pages: {
-  //   signIn: "/auth/signin",
-  // },
   session: { strategy: "jwt" },
+  // jwt: {
+  //   async encode({ secret, token }) {
+  //     return jwt.sign(token, secret);
+  //   },
+  //   async decode({ secret, token }) {
+  //     return jwt.verify(token, secret);
+  //   },
+  // },
 };
 
 export default NextAuth(authOptions);
