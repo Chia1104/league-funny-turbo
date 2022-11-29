c6612f26fca953184fcdf351496d3364bf72bec8
Vivian <pig98765432160@gmail.com>
2022-11-29 09:33:57 +0800

    Update apps/exp-web/src/components/client/UserIntro/SendPrivateMsgModal.tsx
    Co-authored-by: 俞又嘉 <yuyuchia7423@gmail.com>

--- apps/exp-web/src/components/client/UserIntro/SendPrivateMsgModal.tsx
+++ apps/exp-web/src/components/client/UserIntro/SendPrivateMsgModal.tsx
@@ -24,7 +24,7 @@ const SendPrivateMsgModal: FC = () => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6">
+          >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
