fc3c4b33e6a2081841678962f2329617fc47a610
Chia1104 <yuyuchia7423@gmail.com>
2022-11-30 13:11:55 +0800

    :white_check_mark: Update `web` NewPost page e2e testing.

--- apps/web/cypress/e2e/new-post.cy.js
+++ apps/web/cypress/e2e/new-post.cy.js
@@ -2,6 +2,11 @@ describe("New Post page spec", () => {
   beforeEach(() => {
     cy.visit("/new-post");
   });
+  it("New Post should return 200", () => {
+    cy.request("/new-post").then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
   it("Make sure redirect to HomePage when user is not logged in", () => {
     cy.location("pathname").should("eq", "/b");
   });
