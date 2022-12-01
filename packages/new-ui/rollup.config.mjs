62516eebd7e20da79ae02816b774820896da1939
Chia1104 <yuyuchia7423@gmail.com>
2022-12-02 02:05:10 +0800

    :building_construction: Make architectural changes.

--- packages/new-ui/rollup.config.mjs
+++ packages/new-ui/rollup.config.mjs
@@ -3,7 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve as resolve } from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import { babel } from "@rollup/plugin-babel";
-import dts from "rollup-plugin-dts";
 
 export default defineConfig([
   {
