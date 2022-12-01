6f5d1a3e70e0c797429abeca1b2fde16fb2e728d
Chia1104 <yuyuchia7423@gmail.com>
2022-12-01 14:06:43 +0800

    :package: minify `@wanin/shared`.

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ["utils/index.ts", "types/index.ts", "utils/zod-schema/index.ts"],
   clean: true,
   dts: true,
+  minify: true,
   outDir: "dist",
   format: ["cjs", "esm"],
 });
