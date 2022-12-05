f077bc277d04d6840d8e25aac16846a069247372
Chia1104 <yuyuchia7423@gmail.com>
2022-12-05 11:35:38 +0800

    :hammer: Update `IsLogin` component.

--- apps/web/src/components/IsLogin/IsLogin.tsx
+++ apps/web/src/components/IsLogin/IsLogin.tsx
@@ -5,14 +5,25 @@ import { IS_PRODUCTION } from "@/shared/constants";
 interface Props {
   fallBack?: ReactNode;
   children: ReactNode;
-  isLogin?: boolean;
+  debug?: {
+    isLogin?: boolean;
+    isAdmin?: boolean;
+  };
 }
 
 const IsLogin: FC<Props> = (props) => {
-  const { fallBack = null, children, isLogin } = props;
+  const {
+    fallBack = null,
+    children,
+    debug = { isLogin: false, isAdmin: false },
+  } = props;
   const { data: session } = useSession();
 
-  if (!IS_PRODUCTION && isLogin) {
+  if (!IS_PRODUCTION && debug?.isLogin) {
+    return <>{children}</>;
+  }
+
+  if (!IS_PRODUCTION && debug?.isAdmin) {
     return <>{children}</>;
   }
 
