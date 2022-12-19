5315e98225126e96fc76a8b524d9fd8ecf9e19e1
Chia1104 <yuyuchia7423@gmail.com>
2022-12-19 11:18:10 +0800

    :adhesive_bandage: Fix og runtime.

--- apps/web/src/pages/api/services/og/index.tsx
+++ apps/web/src/pages/api/services/og/index.tsx
@@ -3,7 +3,7 @@ import { ImageResponse } from "@vercel/og";
 import { type NextRequest } from "next/server";
 
 export const config = {
-  runtime: "edge",
+  runtime: "experimental-edge",
 };
 
 export default function handler(
